6e1880b514358554073c9c223128edb421544a0f
HalaAbbas21 <119543956+HalaAbbas21@users.noreply.github.com>
2023-07-25 16:13:29 +0300

    Delete lib directory

--- lib/auth/sign up/doctor_signup/filter_list/selected_list_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class SelectedListController extends GetxController{
-  var _selectedList= List<String>.empty(growable:true).obs;
-  getSelectedList() => _selectedList;
-  setSelectedList(List<String> list) => _selectedList.value=list;
-}
